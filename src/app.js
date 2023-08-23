4a2c060a1d1dc4e184c4819ba386ec01a14c0f08
unknown <olzheke02@mail.ru>
2023-08-23 15:47:49 +0600

    pulled project

---
+++ src/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+});
+
+
